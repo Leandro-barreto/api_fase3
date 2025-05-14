4d9b2070c69c53cc12cd72ac2388b2f72e8ab466
Leandro-barreto <leandro.lhbb@gmail.com>
2025-05-14 14:26:35 -0300

    Quebrando função

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, UploadFile, File, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 import joblib
-from services.functions import read_uploaded_file, apply_model, render_result_table
+from .services.functions import read_uploaded_file, apply_model, render_result_table
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
@@ -17,10 +17,10 @@ async def upload_file(request: Request, file: UploadFile = File(...)):
     except Exception as e:
         return HTMLResponse(content=f"<h3>Erro ao ler o arquivo: {str(e)}</h3>", status_code=400)
 
-    try:
-        df_with_preds = apply_model(df, model)
-    except Exception as e:
-        return HTMLResponse(content=f"<h3>{str(e)}</h3>", status_code=500)
+    # try:
+    #     df_with_preds = apply_model(df, model)
+    # except Exception as e:
+    #     return HTMLResponse(content=f"<h3>{str(e)}</h3>", status_code=500)
 
-    html_content = render_result_table(df_with_preds, file.filename)
-    return HTMLResponse(content=html_content)
+    # html_content = render_result_table(df_with_preds, file.filename)
+    # return HTMLResponse(content=html_content)
